23b92bb5d144f7000a4c869b365ef762b96d373f
myoh <myoh@student.42seoul.kr>
2020-08-18 11:27:55 +0900

    추가: div 정리

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -42,45 +42,56 @@ const Login = () => {
             </Btn>
             {/* 엔터키로 로그인 버튼 누르는 기능 가능하게 만듦 */} 
             <form onSubmit={handleSubmit}> 
-            <StyledText1 className="text1">또는</StyledText1>
-            <div className="emailInput">
-              <Input
-              type="text"
-              placeholder="이메일 주소 입력"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              name="email"
-              className={errors.email && 'error-input'}
-              autoComplete="off"
-              value={values.email}
-            />
-            {errors.email && <ErrorText>{errors.email}</ErrorText>}
-            </div>
-            <div className="passwordInput">
-            <Input
-              type="text"
-              placeholder="비밀번호 입력"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={errors.password && 'error-input'}
-              name="password"
-              autoComplete="off"
-              value={values.password}
-            />
-            {errors.password && <ErrorText>{errors.password}</ErrorText>}
-            </div>
+              <StyledText1 className="text1">또는</StyledText1>
+              <div className="email-input">
+                <Input
+                  type="text"
+                  placeholder="이메일 주소 입력"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  name="email"
+                  className={errors.email && 'error-input'}
+                  autoComplete="off"
+                  value={values.email}
+                />
+              {errors.email && <ErrorText>{errors.email}</ErrorText>}
+              </div>
+              <div className="password-input">
+                <Input
+                  type="text"
+                  placeholder="비밀번호 입력"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={errors.password && 'error-input'}
+                  name="password"
+                  autoComplete="off"
+                  value={values.password}
+                />
+              {errors.password && <ErrorText>{errors.password}</ErrorText>}
+              </div>
+              <Space />
+              <Btn type="submit" className="login" color="login" /* onClick={handleSubmit} */ disabled={isSubmitted}>로그인</Btn>
+            </form>
             <Space />
-            <Btn type="submit" className="login" color="login" /* onClick={handleSubmit} */ disabled={isSubmitted}>로그인</Btn>
-             </form>
-            <Space />
-            <StyledText>아직 계정이 없으신가요?</StyledText>
-            <div className="registration"><Signup href="/sign-up"><StyledText1>덕질이 밥먹여준다 가입하기</StyledText1></Signup>
+            <StyledText className="text2">
+              아직 계정이 없으신가요?
+            </StyledText>
+            <div className="registration">
+              <Signup href="/sign-up">
+                <StyledText1>
+                  덕질이 밥먹여준다 가입하기
+                </StyledText1>
+              </Signup>
             </div>
             <Line />
-            <div>
+            <div className="forgot-password">
               <StyledText>
-              <ForgotPassword href="/sign-up"><StyledText1>혹시 비밀번호를 잊으셨나요?</StyledText1></ForgotPassword>
-            </StyledText>
+                <ForgotPassword href="/sign-up">
+                  <StyledText1>
+                    혹시 비밀번호를 잊으셨나요?
+                  </StyledText1>
+                </ForgotPassword>
+              </StyledText>
             </div>
         </Wrapper>
       </Container>
