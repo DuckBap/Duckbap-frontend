280479b81090508ba024ec85f5c4710d82219ea4
hwano <jho2301@gmail.com>
2020-08-18 14:52:07 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import IdolProductList from './components/IdolProductList/IdolProductList';
-import IdolList from './components/IdolList/IdolList';
-import EnterpriseList from './components/IdolList/EnterpriseList';
 
 function App() {
   return (
-    <div className="App">
-      {/* <IdolProductList /> */}
-      <EnterpriseList></EnterpriseList>
-    </div>
+    <div className="App" />
   );
 }
 
