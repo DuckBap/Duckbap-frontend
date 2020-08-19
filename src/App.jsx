6c9e5dee8cfb7507d25858da8cc276b32ca0d4de
hjeon <hjeon@student.42seoul.kr>
2020-08-19 21:02:43 +0900

    기능 추가: 라우터

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import MainPage from './components/MainPage/MainPage';
+import Login from './components/Login';
+import Mypage from './components/Mypage/Mypage';
+import Signup from './components/signup';
+import DetailPage from './components/ProductDetailPage/DetailPage';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Switch>
+        <Route path="/" component={MainPage} exact />
+        <Route path="/login" component={Login} />
+        <Route path={["/mypage","/mypage/:username"]} component={Mypage} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/product/:productNo" component={DetailPage} />
+      </Switch>
+    </div>
   );
 }
 
