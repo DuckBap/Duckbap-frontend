c17c8f5bed288ca284bd5628c113c5b597dfe0eb
yujo <yujo@student.42seoul.kr>
2020-08-20 00:26:52 +0900

    example.json 파일을 추가하고 기존 ProductDetailPage에 import 돼 있던 example.json의 경로를 수정했습니다.

--- src/components/ProductDetailPage/DetailPageSummary.js
+++ src/components/ProductDetailPage/DetailPageSummary.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import example from '../example.json';
+import example from './example.json'
 
 const SummaryBox = styled.div`
   width: 100vw;
