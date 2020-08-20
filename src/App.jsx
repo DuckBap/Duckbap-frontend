e3d1a53bf59ef9786d56bcff37a3fc6090ea9f18
myoh <myoh@student.42seoul.kr>
2020-08-20 14:34:25 +0900

    App.jsx 내용 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
-import Header from './components/Header/Header'
-
-/*export const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap');
-    font-family: 'Nanum Gothic', 'sans-serif' !important;
-`;*/
 
 function App() {
   return (
     <div className="App">
-    {/*  <Header />
-    <MainPage /> */}
     </div>
   );
 }
