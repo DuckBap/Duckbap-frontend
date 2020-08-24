caf7e5be626d2e304437a532d105a3d419c7fa33
seohee <seohee@MacBook-Pro.local>
2020-08-24 22:13:22 +0900

    수정: 마이페이지 스타일

--- src/components/Mypage/OrderHistoryItem.styles.js
+++ src/components/Mypage/OrderHistoryItem.styles.js
@@ -1,46 +1,46 @@
 import styled from 'styled-components';
 
 export const OrderHistoryCard = styled.div`
-	display: flex;
-	width: 100%;
-	height: 200px;
-	justify-content: flex-start;
-	cursor: pointer;
-	border-radius: 10px;
-	margin-bottom: 30px;
-	padding-left: 30px;
+  display: flex;
+  width: 100%;
+  height: 200px;
+  justify-content: flex-start;
+  cursor: pointer;
+  border-radius: 10px;
+  margin-bottom: 30px;
+  padding-left: 30px;
 
-	box-shadow:  0 0 20px rgba(0, 0, 0, 0);
-	transition: all 1250ms cubic-bezier(0.19, 1, 0.22, 1);
-	&:hover {
-		box-shadow:inset 1px 4px 10px -6px rgba(0, 0, 0, 0.3), 0 0 3px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0);
+  transition: all 1250ms cubic-bezier(0.19, 1, 0.22, 1);
+  &:hover {
+    box-shadow: inset 1px 4px 10px -6px rgba(0, 0, 0, 0.3), 0 0 3px rgba(0, 0, 0, 0.1);
 
-		background-color:#F5F5F5;
-	}
-	& img {
-		width: 300px;
-		height: 200px;
-		margin-right: 10px;
-		padding: 10px;
-	}
-	& div {
-		color: rgba(0, 0, 0, 0.7);
-		padding-bottom : 10px;
-		margin-top: 10px;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-around;
-	}
-	& span {
-		line-height: 20px;
-		color: rgba(0,0,0,0.6);
-		padding-right: 40px;
-
-	}
-	& goodsname {
-		font-size: 30px;
-		font-weight: 800;
-		max-width: inherit;
-		color: rgba(0,0,0,0.7);
-	}
+    background-color: #f5f5f5;
+  }
+  & img {
+    height: 200px;
+    margin-right: 10px;
+    padding: 10px;
+  }
+  & div {
+    color: rgba(0, 0, 0, 0.7);
+    padding-bottom: 10px;
+    margin-top: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+  & span {
+    line-height: 20px;
+    color: rgba(0, 0, 0, 0.6);
+    padding-right: 40px;
+    padding-left: 15px;
+    overflow: hidden;
+  }
+  & goodsname {
+    font-size: 30px;
+    font-weight: 800;
+    max-width: inherit;
+    color: rgba(0, 0, 0, 0.7);
+  }
 `;
--- src/components/Mypage/Profile.styles.js
+++ src/components/Mypage/Profile.styles.js
@@ -1,124 +1,123 @@
 import styled from 'styled-components';
 
 export const ProfileContainer = styled.div`
-	width: 1200px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	margin: 20px 0px;
-	//padding: 50px;
-	padding-top: 20px; 
-	padding-bottom: 20px; 
+  width: 1200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0px;
+  //padding: 50px;
+  padding-top: 20px;
+  //padding-bottom: 20px;
 
-	//border: 4px #ccc;
-	//border-radius: 10px;
-	//border-top:1px solid #CACACA;
-	//border-bottom:1px solid #CACACA;
+  //border: 4px #ccc;
+  //border-radius: 10px;
+  //border-top:1px solid #CACACA;
+  //border-bottom:1px solid #CACACA;
 
-	//box-shadow: 3px 5px 10px -6px rgba(0, 0, 0, 0.2);
+  //box-shadow: 3px 5px 10px -6px rgba(0, 0, 0, 0.2);
 `;
 
 export const ProfileColumn = styled.div`
-	display: flex;
-	align-items: flex-start;
-	flex-direction: column;
-	margin-right: 20px;
-	margin-bottom: 10px;
-	&:first-child {
-		cursor: pointer;
-	}
-	&:first-child > div {
-		font-size: large;
-		font-weight: bold;
-		padding: 10px 30px;
-        color: rgba(0,0,0,0.8);
-
-	}
-	&:first-child > img {
-		width: 120px;
-		height: 120px;
-		border-radius: 100%;
-		padding-right: 10px;
-	}
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  margin-right: 20px;
+  margin-bottom: 10px;
+  &:first-child {
+    cursor: pointer;
+  }
+  &:first-child > div {
+    font-size: large;
+    font-weight: bold;
+    padding: 10px 30px;
+    color: rgba(0, 0, 0, 0.8);
+  }
+  &:first-child > img {
+    width: 120px;
+    height: 120px;
+    border-radius: 100%;
+    padding-right: 10px;
+  }
 `;
 
 export const Level = styled.div`
-	font-weight: bold;
-	margin: 10px 5px;
-	cursor: pointer;
-	color: rgba(0,0,0,0.8);
+  font-weight: bold;
+  margin: 10px 5px;
+  cursor: pointer;
+  color: rgba(0, 0, 0, 0.8);
 `;
 
 export const Graph = styled.div`
-	cursor: pointer;
-	background-color: #ccc;
-	//box-shadow: 3px 5px 10px -6px rgba(0, 0, 0, 0.2);
-	height: 30px;
-	width: 100%;
-	border-radius: 40px;
-	margin-bottom: 20px;
-	& > span {
-		display: block;
-		height: 100%;
-		line-height: 28px;
-		text-align: right;
-		width: ${props => props.theme.percent};
-		background-color: #FFC03A;
-		border-radius: 40px;
-		padding: 0 5px;
-		box-sizing: border-box;
-		color: #fff;
-		animation: stack 2s 1;
-	}
-	@keyframes stack {
-		0% {
-			width: 0;
-			color: rgba(255, 255, 255, 0);
-		}
-		50% {
-			color: rgba(255, 255, 255, 1);
-		}
-		100% {
-			width: ${props => props.theme.percent};
-		}
-	}
+  cursor: pointer;
+  background-color: #ccc;
+  //box-shadow: 3px 5px 10px -6px rgba(0, 0, 0, 0.2);
+  height: 30px;
+  width: 100%;
+  border-radius: 40px;
+  margin-bottom: 20px;
+  & > span {
+    display: block;
+    height: 100%;
+    line-height: 28px;
+    text-align: right;
+    width: ${(props) => props.theme.percent};
+    background-color: #ffc03a;
+    border-radius: 40px;
+    padding: 0 5px;
+    box-sizing: border-box;
+    color: #fff;
+    animation: stack 2s 1;
+  }
+  @keyframes stack {
+    0% {
+      width: 0;
+      color: rgba(255, 255, 255, 0);
+    }
+    50% {
+      color: rgba(255, 255, 255, 1);
+    }
+    100% {
+      width: ${(props) => props.theme.percent};
+    }
+  }
 `;
 
 export const FavoriteList = styled.div`
-	width: 600px;
-	height: 30px;
-	display: flex;
-	justify-content: space-around;
-	align-items: center;
+  width: 600px;
+  height: 30px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 export const AddButton = styled.button`
-	width: 50px;
-	height: 50px;
-	border-radius: 100%;
-	outline: 0;
-	border: 0;
-	font-size: xx-large;
-	font-weight: bold;
-	cursor: pointer;
-	box-shadow: 0 0 20px rgba(0, 0, 0, 0);
-	//transition: all 3000ms cubic-bezier(0.19, 1, 0.22, 1);
-	transition: all 1000ms cubic-bezier(0.19, 1, 0.22, 1);
-	&:hover {
-		color:#ffb61a;
-		//box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3), 0 3px 3px rgba(0, 0, 0, 0.1);
-		box-shadow: 0 0 5px rgba(0, 0, 0, 0.3), 0 0 3px rgba(0, 0, 0, 0.1);
-	}
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  outline: 0;
+  border: 0;
+  font-size: xx-large;
+  font-weight: bold;
+  cursor: pointer;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0);
+  //transition: all 3000ms cubic-bezier(0.19, 1, 0.22, 1);
+  transition: all 1000ms cubic-bezier(0.19, 1, 0.22, 1);
+  &:hover {
+    color: #ffb61a;
+    //box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3), 0 3px 3px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.3), 0 0 3px rgba(0, 0, 0, 0.1);
+  }
 `;
 
 export const SmallIdolCard = styled.img`
-	width: 50px;
-	height: 50px;
-	border-radius: 100%;
-	cursor: pointer;
-	box-shadow: 0 0 20px rgba(0, 0, 0, 0);
-	transition: all 1000ms cubic-bezier(0.19, 1, 0.22, 1);
-	&:hover {
-		box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3), 0 3px 3px rgba(0, 0, 0, 0.1);
-	}
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  cursor: pointer;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0);
+  transition: all 1000ms cubic-bezier(0.19, 1, 0.22, 1);
+  &:hover {
+    box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3), 0 3px 3px rgba(0, 0, 0, 0.1);
+  }
 `;
